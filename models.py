f23d1620bde86cc7120deb13fbf9c918b2b6a874
Jerome Rasky <jyrome.112@gmail.com>
2015-12-19 19:47:14 -0800

    Modify database model
    Instead of having a complicated, hand-coded mapping in the serializer.
    Much simpler implementation, and will probably cause less confusion
    later.

--- models.py
+++ models.py
@@ -3,31 +3,31 @@ from django.db import models
 # Create your models here.
 class Registration(models.Model):
     TSHIRT_SIZES = [
-        (0, "extra small"),
-        (1, "small"),
-        (2, "medium"),
-        (3, "large"),
-        (4, "extra large")]
+        ('XS', "extra small"),
+        ('S', "small"),
+        ('M', "medium"),
+        ('L', "large"),
+        ('XL', "extra large")]
     STATUS = [
         (0, "applied"),
         (1, "accepted"),
         (2, "waitlisted"),
         (3, "rejected")]
 
-    email = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
-    university = models.CharField(max_length=200)
-    location = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    school = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
     github = models.CharField(max_length=200, blank=True, null=True)
     linkedin = models.CharField(max_length=200, blank=True, null=True)
-    personal_site = models.CharField(max_length=200, blank=True, null=True)
-    tshirt_size = models.PositiveSmallIntegerField(choices=TSHIRT_SIZES)
-    travel_subsidy = models.BooleanField()
+    personalsite = models.CharField(max_length=200, blank=True, null=True)
+    resume = models.FileField(blank=True, null=True)
+    tshirt = models.CharField(max_length=4, choices=TSHIRT_SIZES)
+    travel_reinbursement = models.BooleanField()
     first_hackathon = models.BooleanField()
     mentor = models.BooleanField()
-    resume = models.FileField(blank=True, null=True)
-    status = models.PositiveSmallIntegerField(choices=STATUS)
     reason = models.TextField()
+    status = models.PositiveSmallIntegerField(choices=STATUS, default=0)
 
     def __str__(self):
         return self.name
--- urls.py
+++ urls.py
@@ -13,74 +13,16 @@ class FormToSerializerBooleanField(serializers.BooleanField):
     TRUE_VALUES = set(('t', 'T', 'true', 'True', 'TRUE', '1', 1, True,'On','on','ON'))
     FALSE_VALUES = set(('f', 'F', 'false', 'False', 'FALSE', '0', 0, 0.0, False,'Off','off','OFF'))
 
-class RegistrationSerializer(serializers.Serializer):
-    name = serializers.CharField(write_only=True)
-    email = serializers.CharField(write_only=True)
-    school = serializers.CharField(write_only=True)
-    city = serializers.CharField(write_only=True)
-    github = serializers.CharField(allow_blank=True, write_only=True)
-    linkedin = serializers.CharField(allow_blank=True, write_only=True)
-    personalsite = serializers.CharField(allow_blank=True, write_only=True)
-    resume = serializers.FileField(required=False, write_only=True)
-    tshirt = serializers.ChoiceField(("S", "M", "L", "XL"), write_only=True)
-    travel_reinbursement = FormToSerializerBooleanField(write_only=True)
-    first_hackathon = FormToSerializerBooleanField(write_only=True)
-    mentor = FormToSerializerBooleanField(write_only=True)
-    reason = serializers.CharField(write_only=True)
+class RegistrationSerializer(serializers.ModelSerializer):
+    travel_reinbursement = FormToSerializerBooleanField()
+    first_hackathon = FormToSerializerBooleanField()
+    mentor = FormToSerializerBooleanField()
 
-    def create(self, validated_data):
-        reg = Registration()
-        reg.email = validated_data.get('email')
-        reg.name = validated_data.get('name')
-        reg.university = validated_data.get('school')
-        reg.location = validated_data.get('city')
-        reg.github = validated_data.get('github')
-        reg.linkedin = validated_data.get('linkedin')
-        reg.personal_site = validated_data.get('personalsite')
-        reg.resume = validated_data.get('resume')
-        tshirt_size = validated_data.get('tshirt')
-        if tshirt_size == 'S':
-            reg.tshirt_size = 1
-        elif tshirt_size == 'M':
-            reg.tshirt_size = 2
-        elif tshirt_size == 'L':
-            reg.tshirt_size = 3
-        elif tshirt_size == 'XL':
-            reg.tshirt_size = 4
-        else:
-            reg.tshirt_size = 2
-        reg.travel_subsidy = validated_data.get('travel_reinbursement')
-        reg.first_hackathon = validated_data.get('first_hackathon')
-        reg.mentor = validated_data.get('mentor')
-        reg.status = 0 # applied
-        reg.reason = validated_data.get('reason')
-        reg.save()
-        return reg
-
-    def update(self, instance, validated_data):
-        instance.email = validated_data.get('email', instance.email)
-        instance.name = validated_data.get('name', instance.name)
-        instance.university = validated_data.get('school', instance.university)
-        instance.location = validated_data.get('city', instance.location)
-        instance.github = validated_data.get('github', instance.github)
-        instance.linkedin = validated_data.get('linkedin', instance.linkedin)
-        instance.personal_site = validated_data.get('personalsite', instance.personal_site)
-        instance.resume = validated_data.get('resume', instance.resume)
-        tshirt_size = validated_data.get('tshirt')
-        if tshirt_size == 'S':
-            instance.tshirt_size = 1
-        elif tshirt_size == 'M':
-            instance.tshirt_size = 2
-        elif tshirt_size == 'L':
-            instance.tshirt_size = 3
-        elif tshirt_size == 'XL':
-            instance.tshirt_size = 4
-        instance.travel_subsidy = validated_data.get('travel_reinbursement', instance.travel_subsidy)
-        instance.first_hackathon = validated_data.get('first_hackathon', instance.first_hackathon)
-        instance.mentor = validated_data.get('mentor', instance.mentor)
-        instance.reason = validated_data.get('reason', instance.reason)
-        instance.save()
-        return instance
+    class Meta:
+        model = Registration
+        fields = ('name', 'email', 'school', 'city', 'github', 'linkedin', 'personalsite',
+                  'resume', 'tshirt', 'travel_reinbursement', 'first_hackathon', 'mentor',
+                  'reason')
 
 class RegistrationViewSet(viewsets.ModelViewSet):
     queryset = Registration.objects.all()
