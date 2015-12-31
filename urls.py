20f790361e371f880be9b55af530fe3546918286
Jerome Rasky <jyrome.112@gmail.com>
2015-12-30 20:18:57 -0800

    Simplify URLs code and remove OPTIONS
    Changes to the form mean we can remove the custom input class

--- urls.py
+++ urls.py
@@ -4,18 +4,7 @@ from rest_framework import routers, serializers, viewsets, mixins
 
 from .models import Registration
 
-class FormToSerializerBooleanField(serializers.BooleanField):
-    ''' workaround to convert django form field to serializer form field
-    see my issue https://github.com/tomchristie/django-rest-framework/issues/2394
-    '''
-    TRUE_VALUES = set(('t', 'T', 'true', 'True', 'TRUE', '1', 1, True,'On','on','ON'))
-    FALSE_VALUES = set(('f', 'F', 'false', 'False', 'FALSE', '0', 0, 0.0, False,'Off','off','OFF'))
-
 class RegistrationSerializer(serializers.ModelSerializer):
-    travel_reinbursement = FormToSerializerBooleanField()
-    first_hackathon = FormToSerializerBooleanField()
-    mentor = FormToSerializerBooleanField()
-
     class Meta:
         model = Registration
         fields = ('name', 'email', 'school', 'city', 'github', 'linkedin', 'personalsite',
@@ -25,6 +14,7 @@ class RegistrationSerializer(serializers.ModelSerializer):
 class RegistrationViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     queryset = Registration.objects.all()
     serializer_class = RegistrationSerializer
+    http_method_names = ['post']
 
 router = routers.SimpleRouter()
 router.register(r'register', RegistrationViewSet)
