08611875dbf7b17b48a3c49f784a2b3c330934f8
Jerome Rasky <jyrome.112@gmail.com>
2016-01-20 08:24:44 -0800

    Add email confirmation on registration

--- urls.py
+++ urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import include, url
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
 
 from rest_framework import routers, serializers, viewsets, mixins
 
@@ -15,6 +17,27 @@ class RegistrationViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     queryset = Registration.objects.all()
     serializer_class = RegistrationSerializer
     http_method_names = ['post']
+
+    def perform_create(self, serializer):
+        # save our instance
+        instance = serializer.save()
+
+        # send confirmation email
+        message = EmailMultiAlternatives("nwHacks 2016 Registration Confirmation",
+                                         render_to_string("nwhacks2016/email.txt", {
+                                             "name": instance.name
+                                         },
+                                         "noreply@nwhacks.io",
+                                         [instance.email],
+                                         reply_to=["apply@nwhacks.io"]))
+
+        # attach html content
+        message.attach_alternative(render_to_string("nwhacks2016/email.html", {
+            "name": instance.name
+        }, "text/html"))
+
+        # send the message
+        message.send()
 
 router = routers.SimpleRouter()
 router.register(r'register', RegistrationViewSet)
